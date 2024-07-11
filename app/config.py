f037f87b9513513238f4c10d8724575aa8884b12
Egore000 <egor.pakhomov.2002@gmail.com>
2024-07-11 16:41:21 +0700

    Refactor

--- app/config.py
+++ app/config.py
@@ -52,11 +52,11 @@ class DBSettings(Config):
 
 
 class TestDBSettings(Config):
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_PASS: int
-    TEST_DB_NAME: str
-    TEST_DB_USER: str
+    TEST_DB_HOST: str | None = None
+    TEST_DB_PORT: int | None = None
+    TEST_DB_PASS: str | None = None
+    TEST_DB_NAME: str | None = None
+    TEST_DB_USER: str | None = None
 
     @property
     def TEST_DATABASE_URL(self):
